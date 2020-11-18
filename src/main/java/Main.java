a0048ba17fb171b782d84d9389b0227db2909a0c
Denis Saenko <dsaenko@yandex.ru>
2020-11-18 17:25:06 +0100

    second try to pull request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("pull request");
+        System.out.println("pull request 2");
         if (args.length == 0) {
             System.err.println(NO_ARGUMENT_MESSAGE);
             System.exit(1);
