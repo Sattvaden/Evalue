3993ee475d391bf34c7430a5c0a9d38f04b70ad5
Denis Saenko <dsaenko@yandex.ru>
2020-11-18 17:57:37 +0100

    out of resoucre folder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("pull request 3");
+        System.out.println("no trigger out of resource folder");
         if (args.length == 0) {
             System.err.println(NO_ARGUMENT_MESSAGE);
             System.exit(1);
