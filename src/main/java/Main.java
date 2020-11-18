32cdbd820bb9d13437bd2b6dd72aa020b21b8232
Denis Saenko <dsaenko@yandex.ru>
2020-11-18 20:27:42 +0100

    hello, leo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello, Leo!");
+        System.out.println("Hello, Leo! You are the best!");
         if (args.length == 0) {
             System.err.println(NO_ARGUMENT_MESSAGE);
             System.exit(1);
