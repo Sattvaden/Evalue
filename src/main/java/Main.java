a99442d368ce431fdafc4729753ecd911ca1d996
ds185438 <72257703+ds185438@users.noreply.github.com>
2020-11-18 11:34:46 +0100

    sout is removed (#1)
    Co-authored-by: Denis Saenko <ds185438@ncr.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from submodule");
         if (args.length == 0) {
             System.err.println(NO_ARGUMENT_MESSAGE);
             System.exit(1);
